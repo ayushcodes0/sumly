8d120b264f781c91ddf60a02200ab0eff8c0ce40
ayushcodes0 <ayushsinghcodes@gmail.com>
2025-04-06 19:46:56 +0530

    added hero section

--- components/home/DemoSection.tsx
+++ components/home/DemoSection.tsx
@@ -1,11 +1,17 @@
+import { Pizza } from 'lucide-react'
 import React from 'react'
 
 const DemoSection = () => {
   return (
     <section className='relative'>
-      <div aria-hidden="true" className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-30">
+      <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div aria-hidden="true" className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-30">
         <div style={{clipPath: 'plygon(74.1%, 44.1% 100% 61.6% 97.5% 26.9% 85.5% 0.1% 80.7% 2% 72.5% 32.5% 60.2% 62.4% 52.4% 68.1% 47.5% 58.3% 45.2% 34.5% 27.5% 76.7% 0.1% 64.9% 17.9% 100% 27.6% 76.8% 76.1% 97.7% 74.1% 44.1%)'}} className="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-linear-to-br from bg-emerald-500 via-teal-500 to-cyan-500 opacity-20 sm:left-[calc(50%-30rem)] sm-w-[72rem]"></div>
-      </div>
+        </div>
+        <div>
+            <Pizza/>
+        </div>
+      </div>  
     </section>
   )
 }
