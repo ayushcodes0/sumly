307d7ff8a01d497ef19f8225c2e6296c0c5e0414
ayushcodes0 <ayushsinghcodes@gmail.com>
2025-04-17 01:04:00 +0530

    Adding pricing section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from "@/components/home/HeroSection"
 import BgGradient from "@/components/common/BgGradient";
 import DemoSection from "@/components/home/DemoSection";
 import HowItWorksSection from "@/components/home/HowItWorksSection";
+import PricingSection from "@/components/home/PricingSection";
 
 export default function Home() {
   return(
@@ -11,6 +12,7 @@ export default function Home() {
         <HeroSection/>
         <DemoSection/>
         <HowItWorksSection/>
+        <PricingSection/> 
       </div>
     </div>
   );
---
+++ components/home/PricingSection.tsx
@@ -0,0 +1,80 @@
+import Link from 'next/link'
+import React from 'react'
+
+const priceType = {
+  name: String,
+  price: Number,
+  description: String,
+  items: String[],
+  id: String,
+  paymentLink: String,
+  priceId: String
+}
+
+const plans = [
+  {
+    name: "Basic",
+    price: 9,
+    description: "Perfect for occasional use",
+    items: [
+      "5 PDF summaries per month",
+      "Standard processing speed",
+      "Email support",
+    ],
+    id: "pro",
+    paymentLink: "",
+    priceId: ""
+  },{
+    name: "Pro",
+    price: 19,
+    description: "For professionals and teams",
+    items: [
+      "Unlimited pdf summaries",
+      "Priority Processing",
+      "24/7 Priority support",
+      "Markdown Export"
+    ],
+    id: "pro",
+    paymentLink: "",
+    priceId: ""
+  }
+]
+
+const PricingCard = ({name , description, price , items, id, paymentLink}: priceType)=>{
+  return <div className='relative w-full max-w-lg'>
+    <div>
+      <p>{name}</p>
+      <p>{description}</p>
+    </div>
+    <div>
+      <p>{price}</p>
+    </div>
+    <div>
+      {items.map((item, idx)=>(
+        <li key={idx}>{item}</li>
+      ))}
+    </div>
+    <div>
+      <Link href={paymentLink}>Buy Now</Link>
+    </div>
+  </div>
+}
+
+const PricingSection = ()=> {
+  return (
+    <section>
+      <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 ">
+        <div>
+          <h2>Pricing</h2>
+        </div>
+        <div className='relative flex justify-center flex-col lg:flex-row items-center lg:items-stretch gap-8'>
+          {plans.map((plan)=>(
+            <PricingCard key={plan.id} {...plan}/>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default PricingSection
