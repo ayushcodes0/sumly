0435cc621e6a28eb3e78e347b64a5f6ea8213359
ayushcodes0 <ayushsinghcodes@gmail.com>
2025-05-15 21:49:48 +0530

    .

--- components/home/DemoSection.tsx
+++ components/home/DemoSection.tsx
@@ -14,6 +14,12 @@ const DemoSection = () => {
             <div className="flex justify-center items-center px-2 sm:px-4 lg:px-6">
                 {/* {Summary Viewer} */}
             </div>
+
+
+
+
+
+            
         </div>
       </div>  
     </section>
